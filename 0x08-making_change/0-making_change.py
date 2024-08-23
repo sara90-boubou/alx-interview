c91fd31087169669019c6976ee69804faeb93f2e
sara90-boubou <saraboubou4@gmail.com>
2024-08-22 17:14:19 -0700

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
+'''DSA Challenge
+'''
 
 
-def make_change(coins, total):
-    """Determines the fewest number of coins needed \
-        to meet a given amount total"""
+def makeChange(coins, total):
+    '''determine the fewest number of coins needed to meet a given amount total
+    '''
     if total <= 0:
         return 0
-
-    current_total = 0
-    used_coins = 0
-    coins = sorted(coins, reverse=True)
-    for coin in coins:
-        r = (total - current_total) // coin
-        current_total += r * coin
-        used_coins += r
-        if current_total == total:
-            return used_coins
-    return -1
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while total > 0:
+        if coin_idx >= n:
+            return -1
+        if total - sorted_coins[coin_idx] >= 0:
+            total -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
